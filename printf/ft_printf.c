fdab9e5354fa220a8d0dda65dec21125aaefa306
Robin Schramm <wobula@gmail.com>
2017-11-04 23:26:14 -0700

    Small change to comment.

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -78,10 +78,10 @@ static void	activate_frankenstein(t_print *ptr, int *xptr)
 }
 
 /*
-** This is our buffer printing function.  We only call write once!
-** Notice how we are still keeping track of our return value.  In the
-** context of hundreds of thousands of characters printing to the screen
-** this tiny little change in code will create massive speed optimizations.
+** This is our buffer printing function.  Notice how we are still keeping 
+** track of our return value.  In the context of hundreds of thousands of
+** characters printing to the screen this tiny little change in code will
+** create massive speed optimizations.
 */
 
 static void print_buffer(char *format, int *ret, int start, int x)
